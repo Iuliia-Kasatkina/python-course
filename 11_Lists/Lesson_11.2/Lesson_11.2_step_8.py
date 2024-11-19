5b64a423256eaa0fd6f567bd5132490d765949cc
kasatkina.iuliia <upastuhova50@gmail.com>
2024-11-19 22:06:28 +0200

    Added files Lesson_11.2_step_2 - Lesson_11.2_step_9

---
+++ 11_Lists/Lesson_11.2/Lesson_11.2_step_8.py
@@ -0,0 +1,16 @@
+# 11.2 Основы работы со списками
+"""
+Дополните приведенный код так, чтобы он вывел "перевёрнутый" список languages (т.е. элементы будут идти в обратном порядке).
+
+Примечание. Для вывода списка воспользуйтесь функцией print().
+
+Исходный код
+
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+
+
+"""
+
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+
+print(languages[::-1])
