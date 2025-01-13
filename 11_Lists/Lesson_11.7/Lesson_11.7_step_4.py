fe8a586ca0f33762c27424735527577ae82dc733
kasatkina.iuliia <upastuhova50@gmail.com>
2025-01-13 13:20:35 +0200

    Added file Lesson_11.7_step_4

---
+++ 11_Lists/Lesson_11.7/Lesson_11.7_step_4.py
@@ -0,0 +1,21 @@
+# 11.7 Списочные выражения
+"""
+Используя списочное выражение, дополните приведённый ниже код так, чтобы получить новый список,
+содержащий длины строк исходного списка.
+
+Исходный код:
+
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+lengths =
+
+print(lengths)
+
+
+"""
+
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+lengths = [len(m) for m in keywords]
+
+print(lengths)
