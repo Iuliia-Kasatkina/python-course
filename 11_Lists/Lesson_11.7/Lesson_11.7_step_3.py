2d02c5a19a9a6b6a98058fd40bb09e71f8306606
kasatkina.iuliia <upastuhova50@gmail.com>
2025-01-13 13:17:50 +0200

    Added file Lesson_11.7_step_3

---
+++ 11_Lists/Lesson_11.7/Lesson_11.7_step_3.py
@@ -0,0 +1,21 @@
+# 11.7 Списочные выражения
+"""
+Используя списочное выражение, дополните приведённый ниже код так, чтобы получить новый список, содержащий строки исходного списка,
+где у каждой строки удалён первый символ.
+
+Исходный код:
+
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+new_keywords =
+
+print(new_keywords)
+
+
+"""
+
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+new_keywords = [m[1:] for m in keywords]
+
+print(new_keywords)
