ccaf0c2f5daf9e328140aa45647ed0c3bf8325c4
kasatkina.iuliia <upastuhova50@gmail.com>
2025-01-17 13:37:14 +0200

    Added file Lesson_13.2_step_8

---
+++ Functions/Lesson_13.2/Lesson_13.2_step_8.py
@@ -0,0 +1,28 @@
+# 13.1 Функции без параметров
+"""
+Звёздный треугольник
+
+Напишите функцию draw_triangle(fill, base), которая принимает два параметра:
+
+fill – символ заполнитель;
+base – величина основания равнобедренного треугольника;
+а затем выводит его.
+
+Примечание. Гарантируется, что основание треугольника – нечётное число.
+
+"""
+
+def draw_triangle(fill, base):
+    count = 0
+    for i in range(1, base+1):
+        if i <= (base+1) // 2:
+            count += 1
+            print(fill * count)
+        else:
+            count -= 1
+            print(fill * count)
+
+fill = input()
+base = int(input())
+
+draw_triangle(fill, base)
